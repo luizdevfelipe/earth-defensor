a68376ba5737a2c27cd64f436a876bf349c0bb41
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-03-16 23:06:25 -0300

    todaas as configurações para serem alteradas

--- src/conf.lua
+++ src/conf.lua
@@ -1,4 +1,41 @@
-function love.conf (t)
-  t.window.width = 1280 
-  t.window.height = 768 
+function love.conf(t)  
+  t.identity = nil
+  t.version = "0.10.1"
+  t.console = false
+  t.accelerometerjoystick = true
+  t.externalstorage = false
+  t.gammacorrect = true
+  
+  t.window.title = "Meu Jogo"
+  t.window.icon = nil
+  t.window.width = 1280
+  t.window.height = 768
+  t.window.borderless = false
+  t.window.resizable = true
+  t.window.minwidth = 800
+  t.window.minheight = 600
+  t.window.fullscreen = false
+  t.window.fullscreentype = "desktop"
+  t.window.vsync = true
+  t.window.msaa = 0
+  t.window.display = 1
+  t.window.highdpi = false
+  t.window.x = nil
+  t.window.y = nil 
+  
+  t.modules.audio = true
+  t.modules.event = true
+  t.modules.graphics = true
+  t.modules.image = true
+  t.modules.joystick = true
+  t.modules.math = true
+  t.modules.mouse = true
+  t.modules.physics = true
+  t.modules.sound = true
+  t.modules.system = true
+  t.modules.timer = true
+  t.modules.touch = true
+  t.modules.video = true
+  t.modules.window = true
+  t.modules.threads = true
 end

123e4bfcad5c90015ae8412f9161f4f21e5138db
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-05-12 22:15:22 -0300

    console desativado

--- src/conf.lua
+++ src/conf.lua
@@ -1,7 +1,7 @@
 function love.conf(t)  
   t.identity = nil
   t.version = "0.10.1"
-  t.console = true
+  t.console = false
   t.accelerometerjoystick = true
   t.externalstorage = false
   t.gammacorrect = true
